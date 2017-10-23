c38250e341c9e708c1292c0b7cf72e55fa01104e
JF Padilla <emailreel@gmail.com>
2017-10-23 13:54:48 -0400

    create SongPlayer service and inject it into AlbumCtrl

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,10 @@
+(function() {
+  function SongPlayer() {
+    let SongPlayer = {};
+    return SongPlayer;
+  }
+
+  angular
+    .module('blocJams')
+    .factory('SongPlayer', SongPlayer);
+})();
