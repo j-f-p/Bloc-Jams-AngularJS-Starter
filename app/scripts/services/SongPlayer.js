8df44eb2b12c524751c4ae33c47bca3eac713022
JF Padilla <emailreel@gmail.com>
2017-10-23 14:53:24 -0400

    add preliminary logic for play and pause button

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,35 @@
 (function() {
   function SongPlayer() {
     let SongPlayer = {};
+    let currentSong = null;
+    let currentBuzzObject = null;
+
+    SongPlayer.play = function(song) {
+      if(currentSong !== song) {
+        if(currentBuzzObject) {
+          currentBuzzObject.stop();
+          currentSong.playing = null;
+        }
+
+        currentBuzzObject = new buzz.sound(song.audioUrl,
+        { formats: ['mp3'], preload: true} );
+
+        currentSong = song;
+
+        currentBuzzObject.play();
+        song.playing = true;
+      } else if (currentSong===song) {
+        if(currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+        }
+      }
+    };
+
+    SongPlayer.pause = function(song) {
+        currentBuzzObject.pause();
+        song.playing = false;
+    };
+
     return SongPlayer;
   }
 
