a448d9380426c1265c90701603bb594113c90384
JF Padilla <emailreel@gmail.com>
2017-10-31 16:46:37 -0400

    refactor time filter to employ buzz time formatter

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,19 +1,7 @@
 (function() {
   function timecode() {
     return function(secondsString) {
-      let seconds = Number.parseFloat(secondsString);
-
-      if(Number.isNaN(seconds)) return '-:--';
-
-      let wholeSeconds = Math.floor(seconds);
-      let minutes = Math.floor(wholeSeconds/60);
-      let remainingSeconds = wholeSeconds % 60;
-
-      let output = minutes + ':';
-      if(remainingSeconds<10) output+='0';
-      output+=remainingSeconds;
-
-      return output;
+      return buzz.toTimer( Number.parseFloat(secondsString) );
     };
   }
 
